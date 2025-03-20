f6cd9e6b40cbe409040b821b7b9fce3ade1f17d6
lmdumpsters <lessmessdumpsters@gmail.com>
2025-03-20 04:28:41 -0400

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+<<<<<<< HEAD
   site: "https://lmdumpsters.com",
+=======
+  site: "https://example.com",
+>>>>>>> parent of 41cea01 (init commit)
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
